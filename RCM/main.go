db611970450f7151abe13ea68ba88e9355983822
AZ's Macbook <zhaoyz@bc.edu>
2018-10-09 14:40:15 -0400

    finished main

---
+++ RCM/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	// "flag"
+    // "sync"
+	"bufio"
+	"strings"
+    "strconv"
+)
+
+var(
+	id int
+    node_type string
+    // mutex = new(sync.mutex)
+	mem_list = make(map[int]string)
+    status bool
+)
+
+func main(){
+    node_type,_ = os.Getenv("type")
+
+    switch node_type {
+        case "server":
+            var node Server
+        case "client":
+            var node Client
+    }
+    // up and running
+    status = true
+
+	// get node id
+	id,_ = strconv.Atoi(os.Getenv("id"))
+	// read config file
+	config, err := os.Open("config.txt")
+    if err != nil {
+        fmt.Print(err)
+        return
+    }
+    scanner := bufio.NewScanner(config)
+    for scanner.Scan() {
+        line := strings.Split(scanner.Text(), " ")
+        id, err := strconv.Atoi(line[0])
+        if err != nil {
+            fmt.Println(err)
+            return
+        }
+        addr := line[1]
+        mem_list[id] = addr
+    }
+    config.Close()
+
+    // start running
+	node.init(len(mem_list))
+	
+	go node.recv()
+
+    switch node_type {
+        case "client":
+            node.userInput()
+        case "server":
+            done := make(chan bool)
+            <- done
+    }
+}
+
+func (clt *Client) userInput(){
+    reader := bufio.NewReader(os.Stdin)
+    for {
+        fmt.Print("->")
+        // handle command line input
+        text,_ := reader.ReadString('\n')
+        text = strings.Replace(text, "\n", "", -1)
+        if strings.HasPrefix(text, "write"){
+            input := strings.SplitN(text, " ", 3)
+            
+            key, err := strconv.Atoi(input[1])
+            if err != nil {
+                fmt.Println(err)
+            }
+            // write
+            clt.write(key, input[2])
+        } else if strings.HasPrefix(text, "read") {
+            key, err := strconv.Atoi(strings.SplitN(text, " ", 2)[1])
+            if err != nil {
+                fmt.Println(err)
+            }
+            // output read
+            fmt.Printf("\t%s\n",clt.read(key))
+        } else {
+            // quit
+            // mutex.Lock()
+            status = false
+            // mutex.Unlock()
+            break
+        }
+    }
+}
+
