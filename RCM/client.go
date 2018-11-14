3d31585a4e1b0991ca2279017e87d2f74317becf
Kishori M Konwar <kishori82@gmail.com>
2018-11-13 21:08:25 -0500

    again testing

--- RCM/client.go
+++ RCM/client.go
@@ -1,86 +1,76 @@
 package main
 
 import (
-	"net"
 	"fmt"
+	"net"
 	"sync"
 )
 
 type ReadBufEntry struct {
-	val 		string
-	vec_clock	[]int
+	val       string
+	vec_clock []int
 }
 
 type Client struct {
-	vec_clock		[]int
-	counter			int
-	counter_lock 	sync.Mutex
-	writer_ts		map[int]map[int][]int
-	writer_ts_lock	sync.RWMutex
-	writer_ts_cond	*sync.Cond
-	readBuf 		map[int]ReadBufEntry
-	readBuf_lock 	sync.Mutex
-	readBuf_cond	*sync.Cond
+	vec_clock      []int
+	counter        int
+	writer_ts      map[int]map[int][]int
+	writer_ts_lock sync.Mutex
+	writer_ts_cond *sync.Cond
+	readBuf        map[int]ReadBufEntry
+	readBuf_lock   sync.Mutex
+	readBuf_cond   *sync.Cond
 }
 
 func (clt *Client) init(group_size int) {
 	// init vector timestamp with length group_size
 	clt.vec_clock = make([]int, group_size)
 	// set vector timestamp to zero
-	for i:= 0; i < group_size; i++ {
+	for i := 0; i < group_size; i++ {
 		clt.vec_clock[i] = 0
 	}
 	clt.counter = 0
-	clt.counter_lock = sync.Mutex{}
 	// init writer_ts as counter(int) - timestamp([]int) pairs
-	clt.writer_ts = make(map[int] map[int][]int)
-	clt.writer_ts_lock = sync.RWMutex{}
+	clt.writer_ts = make(map[int]map[int][]int)
+	clt.writer_ts_lock = sync.Mutex{}
 	clt.writer_ts_cond = sync.NewCond(&clt.writer_ts_lock)
 	// init read buffer as counter(int) - (value, timestamp) tuple (ReadBufEntry) pairs
-	clt.readBuf = make(map[int] ReadBufEntry)
+	clt.readBuf = make(map[int]ReadBufEntry)
 	clt.readBuf_lock = sync.Mutex{}
 	clt.readBuf_cond = sync.NewCond(&clt.readBuf_lock)
 }
 
 func (clt *Client) read(key int) string {
-	clt.counter_lock.Lock()
-	clt.counter += 1
-	temp_counter := clt.counter
-	clt.counter_lock.Unlock()
-	msg := Message{Kind: READ, Key: key, Id: id, Counter: temp_counter, Vec: clt.vec_clock}
+	msg := Message{Kind: READ, Key: key, Id: id, Counter: clt.counter, Vec: clt.vec_clock}
 	broadcast(&msg)
 	clt.readBuf_cond.L.Lock()
-	entry, isIn := clt.readBuf[temp_counter]
+	entry, isIn := clt.readBuf[clt.counter]
 	for !isIn {
-		fmt.Println("read waiting...")
 		clt.readBuf_cond.Wait()
-		entry, isIn = clt.readBuf[temp_counter]
+		entry, isIn = clt.readBuf[clt.counter]
 	}
 	clt.readBuf_cond.L.Unlock()
 	clt.merge_clock(entry.vec_clock)
+	clt.counter += 1
 	return entry.val
 }
 
 func (clt *Client) write(key int, value string) {
 	clt.vec_clock[id] += 1
-	clt.counter_lock.Lock()
-	clt.counter += 1
-	temp_counter := clt.counter
-	clt.counter_lock.Unlock()
-	msg := Message{Kind: WRITE, Key: key, Val: value, Id: id, Counter: temp_counter, Vec: clt.vec_clock}
+	msg := Message{Kind: WRITE, Key: key, Val: value, Id: id, Counter: clt.counter, Vec: clt.vec_clock}
 	broadcast(&msg)
 	clt.writer_ts_cond.L.Lock()
-	for len(clt.writer_ts[temp_counter]) <= F {
-		fmt.Println("write waiting...")
+	for len(clt.writer_ts[clt.counter]) <= F {
 		clt.writer_ts_cond.Wait()
 	}
 	// fmt.Println(clt.writer_ts[clt.counter])
-	vec_set := clt.writer_ts[temp_counter]
+	vec_set := clt.writer_ts[clt.counter]
 	// merge all elements of writer_ts[counter] with local vector clock
-	for _,vec := range vec_set {
+	for _, vec := range vec_set {
 		clt.merge_clock(vec)
 	}
 	clt.writer_ts_cond.L.Unlock()
+	clt.counter += 1
 }
 
 // Actions to take if receive RESP message
@@ -94,32 +84,26 @@ func (clt *Client) recvRESP(counter int, val string, vec []int) {
 
 // Actions to take if receive ACK message
 func (clt *Client) recvACK(counter int, vec []int) {
-	clt.writer_ts_lock.RLock()
-	_, isIn := clt.writer_ts[counter]
-	clt.writer_ts_lock.RUnlock()
-	if !isIn {
-		clt.writer_ts_lock.Lock()
-		clt.writer_ts[counter] = make(map[int] []int)
-		clt.writer_ts_cond.Broadcast()
-		clt.writer_ts_lock.Unlock()
+	clt.writer_ts_lock.Lock()
+	if _, isIn := clt.writer_ts[counter]; !isIn {
+		clt.writer_ts[counter] = make(map[int][]int)
 	}
 	fmt.Println("ACK message received vec", vec)
-	clt.writer_ts_lock.Lock()
 	clt.writer_ts[counter][len(clt.writer_ts[counter])] = vec
 	clt.writer_ts_cond.Broadcast()
 	clt.writer_ts_lock.Unlock()
 }
 
 // Client listener
-func (clt *Client) recv(){
+func (clt *Client) recv() {
 	// resolve for udp address by membership list and id
-	udpAddr,err1 := net.ResolveUDPAddr("udp4", mem_list[id])
+	udpAddr, err1 := net.ResolveUDPAddr("udp4", mem_list[id])
 	if err1 != nil {
 		fmt.Println("address not found")
 	}
 
 	// create listner socket by address
-	conn,err2 := net.ListenUDP("udp", udpAddr)
+	conn, err2 := net.ListenUDP("udp", udpAddr)
 	if err2 != nil {
 		fmt.Println("address can't listen")
 	}
@@ -131,7 +115,7 @@ func (clt *Client) recv(){
 		go func() {
 			//buffer size is 1024 bytes
 			buf := make([]byte, 1024)
-			num,_,err3 := conn.ReadFromUDP(buf)
+			num, _, err3 := conn.ReadFromUDP(buf)
 			if err3 != nil {
 				fmt.Println(err3)
 			}
@@ -159,7 +143,7 @@ func (clt *Client) merge_clock(vec []int) {
 		fmt.Println(vec)
 		panic("vector clocks are of different lengths")
 	}
-	for i:=0; i<len(clt.vec_clock); i++ {
+	for i := 0; i < len(clt.vec_clock); i++ {
 		if vec[i] > clt.vec_clock[i] {
 			clt.vec_clock[i] = vec[i]
 		}
--- RCM/server.go
+++ RCM/server.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-	"net"
 	"fmt"
+	"net"
 	"sync"
 )
 
 type WitnessEntry struct {
-	id 		int
+	id      int
 	counter int
 }
 
 type Server struct {
-	update_needed 	chan bool
-	m_data			map[int]string
-	m_data_lock		sync.RWMutex
-	vec_clock		[]int
-	vec_clock_lock	sync.RWMutex
-	vec_clock_cond	*sync.Cond
-	queue			Queue
-	witness 		map[WitnessEntry]int
-	witness_lock	sync.Mutex
+	update_needed  chan bool
+	m_data         map[int]string
+	m_data_lock    sync.RWMutex
+	vec_clock      []int
+	vec_clock_lock sync.Mutex
+	vec_clock_cond *sync.Cond
+	queue          Queue
+	witness        map[WitnessEntry]int
+	witness_lock   sync.Mutex
 }
 
 func (svr *Server) init(group_size int) {
 	svr.update_needed = make(chan bool, 100)
 	// init data as key(int)-value(string) pair
-	svr.m_data = make(map[int] string)
+	svr.m_data = make(map[int]string)
 	svr.m_data_lock = sync.RWMutex{}
 	// init vector timestamp with length group_size
 	svr.vec_clock = make([]int, group_size)
-	svr.vec_clock_lock = sync.RWMutex{}
+	svr.vec_clock_lock = sync.Mutex{}
 	svr.vec_clock_cond = sync.NewCond(&svr.vec_clock_lock)
 	// set vector timestamp to zero
-	for i:= 0; i < group_size; i++ {
+	for i := 0; i < group_size; i++ {
 		svr.vec_clock[i] = 0
 	}
 	// init queue
 	svr.queue.Init()
 	// init witness
-	svr.witness = make(map[WitnessEntry] int)
+	svr.witness = make(map[WitnessEntry]int)
 	svr.witness_lock = sync.Mutex{}
 }
 
 // Actions to take if server receives READ message
-func (svr *Server) recvRead(key int, id int, counter int, vec_i []int){
+func (svr *Server) recvRead(key int, id int, counter int, vec_i []int) {
 	// wait until t_server is greater than t_i
 	svr.vec_clock_cond.L.Lock()
 	for !smallerEqualExceptI(vec_i, svr.vec_clock, 999999) {
@@ -99,15 +99,15 @@ func (svr *Server) recvUpdate(key int, val string, id int, counter int, vec_i []
 }
 
 // Server listener
-func (svr *Server) recv(){
+func (svr *Server) recv() {
 	// resolve for udp address by membership list and id
-	udpAddr,err1 := net.ResolveUDPAddr("udp4", mem_list[id])
+	udpAddr, err1 := net.ResolveUDPAddr("udp4", mem_list[id])
 	if err1 != nil {
 		fmt.Println("address not found")
 	}
 
 	// create listner socket by address
-	conn,err2 := net.ListenUDP("udp", udpAddr)
+	conn, err2 := net.ListenUDP("udp", udpAddr)
 	if err2 != nil {
 		fmt.Println("address can't listen")
 	}
@@ -119,7 +119,7 @@ func (svr *Server) recv(){
 		go func() {
 			//buffer size is 1024 bytes
 			buf := make([]byte, 1024)
-			num,_,err3 := conn.ReadFromUDP(buf)
+			num, _, err3 := conn.ReadFromUDP(buf)
 			if err3 != nil {
 				fmt.Println(err3)
 			}
@@ -128,7 +128,7 @@ func (svr *Server) recv(){
 		}()
 
 		msg := <-c
-		
+
 		switch msg.Kind {
 		case READ:
 			// fmt.Println("server receives READ message with vec_clock", msg.Vec)
@@ -144,7 +144,7 @@ func (svr *Server) recv(){
 }
 
 // infinitely often update the local storage
-func (svr *Server) update(){
+func (svr *Server) update() {
 	msg := svr.queue.Dequeue()
 	if msg != nil {
 		// fmt.Println("server receives msg with vec_clock: ", msg.Vec)
@@ -173,7 +173,7 @@ func smallerEqualExceptI(vec1 []int, vec2 []int, i int) bool {
 	if len(vec1) != len(vec2) {
 		panic("vector clocks are of different lengths")
 	}
-	for index:=0; index<len(vec1); index++ {
+	for index := 0; index < len(vec1); index++ {
 		if index == i {
 			continue
 		}
