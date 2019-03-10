a401c386e4465dbce51084ce9c71be212e3bc63c
AZ's Macbook <zhaoyz@bc.edu>
2019-03-10 18:39:50 -0400

    added another implementation of func recvRESP

--- Byz_Fast-RCM/client.go
+++ Byz_Fast-RCM/client.go
@@ -10,20 +10,27 @@ import (
 type ReadBufKey struct {
 	counter 	int
 	val 		string
-	// vec_clock	[]int
+}
+
+type ReadBufValue struct {
+	val 		string
+	number 		int
+	vec_clock	[]int
 }
 
 type Client struct {
 	vec_clock		[]int
 	counter			int
 	counter_lock	sync.Mutex
-	readBuf 		map[ReadBufKey]int
+	// readBuf 		map[ReadBufKey]int
+	readBuf 		map[ReadBufKey]ReadBufValue
 	readBuf_lock	sync.Mutex
 	writeBuf		map[int][]int
 	writeBuf_lock	sync.Mutex
 	writeBuf_cond	*sync.Cond
 	localBuf		map[int]string
-	val_chan		chan string
+	// val_chan		chan string
+	val_chan 		chan ReadBufValue
 }
 
 func (clt *Client) init(group_size int) {
@@ -35,22 +42,36 @@ func (clt *Client) init(group_size int) {
 	clt.counter = 0
 	clt.counter_lock = sync.Mutex{}
 	// init read buffer as counter(int) - (value, timestamp) tuple (ReadBufKey) pairs
-	clt.readBuf = make(map[ReadBufKey]int)
+	// clt.readBuf = make(map[ReadBufKey]int)
+	clt.readBuf = make(map[ReadBufKey]ReadBufValue)
 	clt.readBuf_lock = sync.Mutex{}
 	clt.writeBuf = make(map[int] []int)
 	clt.writeBuf_lock = sync.Mutex{}
 	clt.writeBuf_cond = sync.NewCond(&clt.writeBuf_lock)
 	clt.localBuf = make(map[int] string)
 
-	clt.val_chan = make(chan string)
+	// clt.val_chan = make(chan string)
+	clt.val_chan = make(chan ReadBufValue)
 }
 
 func (clt *Client) read(key int) string {
 	msg := Message{Kind: READ, Key: key, Id: id, Counter: clt.counter, Vec: clt.vec_clock}
 	broadcast(&msg)
-	val := <-clt.val_chan
-	fmt.Println("finish reading")
-	return val
+	readBuf_val := <-clt.val_chan
+	clt.counter += 1
+	if smallerEqualExceptI(readBuf_val.vec_clock, clt.vec_clock, 999999) {
+		val, isIn := clt.localBuf[key]
+		if !isIn {
+			panic("value is not in local buffer")
+			return "Error"
+		}
+		fmt.Println("finish reading")
+		return val
+	} else {
+		clt.merge_clock(readBuf_val.vec_clock)
+		fmt.Println("finish reading")
+		return readBuf_val.val
+	}
 }
 
 func (clt *Client) write(key int, value string) {
@@ -70,40 +91,67 @@ func (clt *Client) write(key int, value string) {
 	// return WRITE-ACK
 }
 
+// receive RESP method 1: do the checking in recvRESP function rather than in recv function
+// func (clt *Client) recvRESP(counter int, key int, val string, vec []int) {
+// 	clt.counter_lock.Lock()
+
+// 	if (clt.counter == counter) {
+// 		if smallerEqualExceptI(vec, clt.vec_clock, 999999) {
+// 			val, isIn := clt.localBuf[key]
+// 			if !isIn {
+// 				panic("value is not in local buffer")
+// 			}
+// 			clt.counter += 1
+// 			clt.val_chan <- val
+// 			fmt.Println("sent")
+// 		} else {
+// 			entry := ReadBufKey{counter: counter, val: val}
+// 			clt.readBuf_lock.Lock() 
+// 			if _, isIn := clt.readBuf[entry]; isIn {
+// 				clt.readBuf[entry] += 1
+// 			} else {
+// 				clt.readBuf[entry] = 1
+// 			}
+// 			buf_num := clt.readBuf[entry]
+// 			clt.readBuf_lock.Unlock()
+
+// 			clt.merge_clock(vec)
+
+// 			if buf_num == F+1 {
+// 				clt.counter += 1
+// 				clt.val_chan <- val
+// 			}
+// 		}
+// 	}
+
+// 	clt.counter_lock.Unlock()
+// }
+
+// receive RESP method 2: do the checking in recv function
 func (clt *Client) recvRESP(counter int, key int, val string, vec []int) {
-	clt.counter_lock.Lock()
+	// clt.counter_lock.Lock()
 
 	if (clt.counter == counter) {
-		if smallerEqualExceptI(vec, clt.vec_clock, 999999) {
-			val, isIn := clt.localBuf[key]
-			if !isIn {
-				panic("value is not in local buffer")
-			}
-			clt.counter += 1
-			clt.val_chan <- val
-			fmt.Println("sent")
+		readBuf_key := ReadBufKey{counter: counter, val: val}
+		clt.readBuf_lock.Lock()
+		if _, isIn := clt.readBuf[readBuf_key]; isIn {
+			temp := clt.readBuf[readBuf_key]
+			temp.number += 1
+			clt.readBuf[readBuf_key] = temp
 		} else {
-			entry := ReadBufKey{counter: counter, val: val}
-			clt.readBuf_lock.Lock() 
-			if _, isIn := clt.readBuf[entry]; isIn {
-				clt.readBuf[entry] += 1
-			} else {
-				clt.readBuf[entry] = 1
-			}
-			buf_num := clt.readBuf[entry]
-			clt.readBuf_lock.Unlock()
-
-			clt.merge_clock(vec)
-
-			if buf_num == F+1 {
-				clt.counter += 1
-				clt.val_chan <- val
-			}
+			clt.readBuf[readBuf_key] = ReadBufValue{val: val, number: 1, vec_clock: vec}
 		}
+		buf_temp := clt.readBuf[readBuf_key]
+		clt.readBuf_lock.Unlock()
+
+		if buf_temp.number == F+1 {
+			clt.val_chan <- buf_temp
+ 		}
 	}
 
-	clt.counter_lock.Unlock()
+	// clt.counter_lock.Unlock()
 }
+
 func (clt *Client) recvACK(counter int, vec []int) {
 	// if _, isIn := clt.writeBuf[counter]; isIn {
 	// 	panic("write operation " + strconv.Itoa(counter) + " is already in the write buffer")
